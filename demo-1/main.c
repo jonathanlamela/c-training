caecc4f42131d9a7f4f31649a093742249a2d92f
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-12-28 22:28:05 +0100

    tests

--- demo-1/main.c
+++ demo-1/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-    return a + b;
-}
+int sum(int a, int b) { return a + b; }
 
-int min(int a, int b) {
-  return a - b;
-}
+int min(int a, int b) { return a - b; }
 
 int main()
 {
-    int x = 5;
-    int y = 10;
-    int result = sum(x, y);
-    printf("The sum of %d and %d is %d\n", x, y, result);
-    return 0;
+  int x = 5;
+  int y = 12;
+  int result = sum(x, y);
+  printf("The sum of %d and %d is %d\n", x, y, result);
+  char *z = "ciao mondo";
+
+  printf("%s\n", z);
+
+  return 0;
 }
