e7c5abcfa1ca4e3da7489d617f2505bb1e3afcca
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-12-29 11:13:45 +0100

    demo 01

---
+++ demo-01/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int mutiply(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    int a = 2;
+    int b = 3;
+    printf("Sum: %d\n", sum(a, b));
+    printf("Multiply: %d\n", mutiply(a, b));
+
+    return 0;
+}
